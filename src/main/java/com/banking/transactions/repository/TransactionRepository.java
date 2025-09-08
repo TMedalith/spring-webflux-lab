827f1a61889aa0065b8ed4631e6786d6dda448cf
TMedalith <u20211f955@upc.edu.pe>
2025-09-07 23:16:27 -0500

    feat: added transaction repository

--- src/main/java/com/banking/transactions/repository/TransactionRepository.java
+++ src/main/java/com/banking/transactions/repository/TransactionRepository.java
@@ -1,13 +1,17 @@
 package com.banking.transactions.repository;
 
-import com.banking.transactions.model.Transaction;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+
+import com.banking.transactions.model.Transaction;
+
 import reactor.core.publisher.Flux;
 
 
 //No se si esto es asi, voy a ver la clase
 @Repository
 public interface TransactionRepository extends ReactiveMongoRepository<Transaction, String> {
-    Flux<Transaction> findByAccountIdOrderByDate(String accountId);
+    Flux<Transaction> findBySourceAccountIdOrderByDate(String sourceAccountId);
+    Flux<Transaction> findByDestinationAccountIdOrderByDate(String destinationAccountId);
+    Flux<Transaction> findBySourceAccountIdOrDestinationAccountIdOrderByDateDesc(String sourceAccountId, String destinationAccountId);
 }
